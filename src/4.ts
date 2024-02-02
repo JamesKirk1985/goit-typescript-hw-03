0fb8ac4d29c0bb77236f404d1278ee900541444e
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2024-02-02 03:17:04 +0200

    finisied

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+abstract class House {
+  constructor() {}
+  door: boolean;
+  key: Key;
+  tenants: Person[];
+
+  comeIn(person: Person) {
+    if (this.door === true) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key);
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+  openDoor(key: Key) {
+    if (key === this.key) {
+      this.door === true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +49,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
